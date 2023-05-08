15a1426e7f106f8e899b1ae4b0cd3650404281e1
Muskan <102637258+Fun-25@users.noreply.github.com>
2023-05-08 10:34:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+//your JS code here. If required
+function helloWorld() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+}
+
+helloWorld().then((result) => {
+  const outputDiv = document.getElementById('output');
+  outputDiv.textContent = result;
+});
